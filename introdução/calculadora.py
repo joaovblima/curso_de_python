e16febd0f222f4e51098029552f6f786a2187131
João Lima <joaovictorbernardinodelima@gmail.com>
2026-02-04 20:04:46 -0300

    exemplos com inteiros e operações usando jupyter

---
+++ introdução/calculadora.py
@@ -0,0 +1,14 @@
+# %%
+#soma 
+print("1 + 1 = ", 1 + 1)
+
+# %%
+print("está forma de escrever string é bem interesante", "funciona legal")
+
+# %%
+#uma coisa que nao lembrava, se eu quiser somente divisão inteira eu passo duas barras 
+print("divisao inteira 10 // 3 = ", 10 //3 )
+
+# %%
+#potencia 
+print("3 ** 4  = ", 3 ** 4)
--- introdução/ola_mundo.py
+++ introdução/ola_mundo.py
@@ -1,3 +1,6 @@
 print("e ai carai")
 print("vamos lá")
 print("revisando python pela 120938103918290 vez")
+
+# %%
+print("teste jupyter")
