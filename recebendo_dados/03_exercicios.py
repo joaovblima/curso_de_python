1db35eef8d293dfec0c93663ade5d4e207e71d7e
João Lima <joaovictorbernardinodelima@gmail.com>
2026-02-04 20:52:55 -0300

    adiciona exercicios propostos

---
+++ recebendo_dados/03_exercicios.py
@@ -0,0 +1,36 @@
+#%%
+print("bom dia!")
+
+#%%
+print("Bom dia")
+nome = input("Qual é o seu nome? ")
+print("que prazer te conhecer", nome)
+
+#%%
+print("História:")
+primeiro_paragrafo = "O senhor é rei"
+segundo_paragrafo = "O senhor é meu pastor e rei"
+terceiro_paragrafo = "Quando eu vacilar, eu nao temerei, pois o senhor está comigo"
+quarto_paragrafo = "Onde ele está? "
+quinto_paragrafo = "Está no céu, está no mar, na extenção do infinito"
+print(primeiro_paragrafo)
+input()
+print(segundo_paragrafo)
+input()
+print(terceiro_paragrafo)
+input()
+print(quarto_paragrafo)
+input()
+print(quinto_paragrafo)
+#%%
+print("calculando raiz quadrada")
+numero = input("Digite um numero para saber sua raiz quadrada: ")
+numero= int(numero)
+raiz_numero = numero ** 0.5
+print("a raiz quadrada de", numero, "é", raiz_numero)
+
+#%%
+numero = input("digite um numero para saber o dobro: ")
+numero = int(numero)
+dobro = numero * 2
+print("o dobro de", numero, "é", dobro)
