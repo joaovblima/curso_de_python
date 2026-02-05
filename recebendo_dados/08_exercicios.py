14165606f58cd0cb0b07e682e8714b6b10b0cf88
João Lima <joaovictorbernardinodelima@gmail.com>
2026-02-05 09:49:17 -0300

    adiciona mais uma funcionalidade ao exercicio

--- recebendo_dados/08_exercicios.py
+++ recebendo_dados/08_exercicios.py
@@ -1,11 +1,17 @@
 # %%
 print("BEM-VINDO A VENDINHA")
-print("ÁGUA MINERAL NATURAL R$ 1,40 - 1")
+print("ÁGUA MINERAL NATURAL R$ 1,50 - 1")
 print("ÁGUA MINERAL COM GÁS R$ 2,50 - 2")
 escolha = input("escolha sua compra: ")
 escolha = int(escolha)
+escolha_quantidade = input("selecione a quantidade: ")
+escolha_quantidade = int(escolha_quantidade)
 
-if escolha ==1: 
-    print("Você vai pagar R$ 1,50")
+valor_total = 0
+
+if escolha == 1:
+    valor_total =  1.5 * escolha_quantidade
+    print("Você vai pagar R$", valor_total)
 else: 
-    print("Você vai pagar R$ 2,50")
+    valor_total =  2.5 * escolha_quantidade
+    print("Você vai pagar R$", valor_total)
