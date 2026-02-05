0657bde0203a30939d336fcfd2b7ec1d4cdbb0af
João Lima <joaovictorbernardinodelima@gmail.com>
2026-02-04 23:21:28 -0300

    adiciona exercicio simples

---
+++ recebendo_dados/08_exercicios.py
@@ -0,0 +1,11 @@
+# %%
+print("BEM-VINDO A VENDINHA")
+print("ÁGUA MINERAL NATURAL R$ 1,40 - 1")
+print("ÁGUA MINERAL COM GÁS R$ 2,50 - 2")
+escolha = input("escolha sua compra: ")
+escolha = int(escolha)
+
+if escolha ==1: 
+    print("Você vai pagar R$ 1,50")
+else: 
+    print("Você vai pagar R$ 2,50")
