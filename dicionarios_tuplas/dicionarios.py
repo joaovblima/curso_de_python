3b9bd6cab7ecaa85bef4756978f6bf168ce8e1e6
João Lima <joaovictorbernardinodelima@gmail.com>
2026-02-24 12:35:36 -0300

    add exemplos com dicionarios

---
+++ dicionarios_tuplas/dicionarios.py
@@ -0,0 +1,27 @@
+dados_joao = {
+    "nome": "joao", 
+    "sobrenome": "lima", 
+    "idade": 29,
+}
+
+dados_teo = {
+    "nome": "teo", 
+    "filhos": True, 
+    "formacao": ["estatistica", "bigdata, datascience"],
+    "cargos": [
+        {"nome": "ds jr", "empresa": "tapps"}, 
+        {"nome": "ds pl", "empresa": "sasa"},
+        {"nome": "ds sr", "empresa": "boticario"},
+        {"nome": "ds espec", "empresa": "via-varejo"}
+    ], 
+}
+
+#print(dados_joao)
+#print(dados_teo)
+#print(dados_teo["formacao"][-1])
+#print(dados_teo["cargos"][-1]["empresa"])
+#print(dados_teo.keys())
+#print(dados_teo.values())
+
+for k, v in dados_teo.items():
+    print(k, v)
