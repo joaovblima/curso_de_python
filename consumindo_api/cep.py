3110a6f4391d5944bbbea53b9e801eeaa7805a4c
João Lima <joaovictorbernardinodelima@gmail.com>
2026-03-02 12:56:50 -0300

    adiciona exercicio que consume uma api e retorna em um arquivo os dados em formato de json

---
+++ consumindo_api/cep.py
@@ -0,0 +1,27 @@
+import json
+import requests
+ceps = [
+    "57800000",
+    "57880000", 
+    "57890000",
+]
+
+url = "https://viacep.com.br/ws/{cep}/json/"
+# cria-se uma lista vazia
+dados = []
+#um loop na lista de ceps criado acima
+for i in ceps:
+    #aqui com um get eu pego as informacoes formatando, dou a informação que cep é o elemento i da minha lista de ceps
+    resposta = requests.get(url.format(cep=i))
+    
+    #se a resposta for 200 eu adiciono a lista
+    if resposta.status_code == 200:
+        dados.append(resposta.json())
+        
+
+with open("ceps.json", "w", encoding="utf-8") as of:
+    json.dump(dados, of, ensure_ascii=False, indent=4)
+
+
+
+
